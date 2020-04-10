408ad83c44c221fb2e28c439890c9aa02a818e11
Thibaut <thibaut.vaillant@polytechnique.edu>
2020-04-10 17:58:16 +0200

    taille ligand

--- ComplexAnalyzer/source/SEComplexAnalyzerApp.cpp
+++ ComplexAnalyzer/source/SEComplexAnalyzerApp.cpp
@@ -22,11 +22,7 @@ SEComplexAnalyzerAppGUI* SEComplexAnalyzerApp::getGUI() const { return static_ca
 
 void SEComplexAnalyzerApp::analyze() {
 
-	QString folderPath = "C:\\Users\\StephaneRedon\\Desktop\\INF473N\\PDB\\1a1e\\";
-	QDir folder(folderPath);
-
-	QStringList pdbFiles = folder.entryList(QStringList() << "*.pdb" << "*.PDB", QDir::Files);
-	QStringList sdfFiles = folder.entryList(QStringList() << "*.sdf" << "*.SDF", QDir::Files);
+	QString folderPath = "D:\\INF473N\\refined-set\\";
 
 	SBList<std::string>* parameters = new SBList<std::string>();
 	parameters->push_back("0");
@@ -38,38 +34,79 @@ void SEComplexAnalyzerApp::analyze() {
 	parameters->push_back("0");
 	parameters->push_back("0");
 
-	foreach(QString filename, sdfFiles) {
+	QDir folder(folderPath);
+	QStringList dirnames = folder.entryList(QDir::AllDirs);
+	int a = folder.count();
+	int b = 0;
+	SBIAPosition3 boxmax = SBIAPosition3::empty;
 
-		SBPointer<SBFolder> folder = new SBFolder("Folder");
-		folder->create();
 
-		SAMSON::disableHolding();
-		SAMSON::importFromFile((folderPath + filename).toStdString(), parameters, folder());
-		SAMSON::enableHolding();
+	foreach(QString dir, dirnames) {
 
-		// get atoms
+		QStringList sdfFiles = QDir(folderPath + dir).entryList(QStringList() << "*.sdf" << "*.SDF", QDir::Files);
 
-		SBNodeIndexer atomIndexer;
-		folder->getNodes(atomIndexer, SBNode::IsType(SBNode::Atom));
 
-		SBIAPosition3 boundingBox = SBIAPosition3::empty;
+		foreach(QString filename, sdfFiles) {
+			b++;
+			SBPointer<SBFolder> folder = new SBFolder("Folder");
+			folder->create();
 
-		SB_FOR(SBNode * node, atomIndexer) {
+			SAMSON::disableHolding();
+			SAMSON::importFromFile((folderPath + dir +"\\"+ filename).toStdString(), parameters, folder());
+			SAMSON::enableHolding();
 
-			SBAtom* atom = static_cast<SBAtom*>(node);
-			boundingBox.bound(atom->getPosition());
+			// get atoms
+
+			SBNodeIndexer atomIndexer;
+			folder->getNodes(atomIndexer, SBNode::IsType(SBNode::Atom));
+
+			SBIAPosition3 boundingBox = SBIAPosition3::empty;
+
+			SB_FOR(SBNode * node, atomIndexer) {
+
+				SBAtom* atom = static_cast<SBAtom*>(node);
+				boundingBox.bound(atom->getPosition());
+
+			}
+			
+
+
+			
+			if (boundingBox.i[0].diameter() > boxmax.i[0].diameter()) {
+
+				boxmax.i[0]=boundingBox.i[0];
+
+			}
+
+			if (boundingBox.i[1].diameter() > boxmax.i[1].diameter()) {
+
+				boxmax.i[1] = boundingBox.i[1];
+
+			}
+
+			if (boundingBox.i[2].diameter() > boxmax.i[2].diameter()) {
+
+				boxmax.i[2] = boundingBox.i[2];
+
+			}
+
+
+			std::cout << b << "sur " + a << std::endl;
+
+			// clean up
+
+			folder.deleteReferenceTarget();
 
 		}
-
-		std::cout << boundingBox.i[0].diameter() << std::endl;
-		std::cout << boundingBox.i[1].diameter() << std::endl;
-		std::cout << boundingBox.i[2].diameter() << std::endl;
-		std::cout << std::endl;
-
-		// clean up
-
-		folder.deleteReferenceTarget();
-
+	
 	}
+
+	std::cout << boxmax.i[0].diameter() << std::endl;
+	std::cout << boxmax.i[1].diameter() << std::endl;
+	std::cout << boxmax.i[2].diameter() << std::endl;
+	std::cout << std::endl;
+
+
+
 
 }
